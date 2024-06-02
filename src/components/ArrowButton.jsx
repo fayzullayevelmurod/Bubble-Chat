743ba97a854b284830d01037a4d1b23da28efdd7
SardorQobilov <sardorqobilov555@gmail.com>
2024-06-02 19:31:27 +0500

    seen__stories height added

--- src/components/ArrowButton.jsx
+++ src/components/ArrowButton.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import assets from "../assets"
 import "./index.css";
+// eslint-disable-next-line react/prop-types, no-unused-vars
 export const ArrowButton = ({ nextIcon, prevIcon, path }) => {
     return (
         <Link className="arrow__btn" to={path}>
